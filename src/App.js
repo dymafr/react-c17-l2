b2d5a9b5be487a1f8325b5edf6ed978a4475c4b1
dymafr <32801166+dymafr@users.noreply.github.com>
2022-10-06 16:15:35 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import Homepage from './pages/Homepage/Homepage';
-import Profile from './pages/Profile/Profile';
+import { Outlet } from 'react-router-dom';
 import styles from './App.module.scss';
 
 function App() {
@@ -10,8 +9,7 @@ function App() {
     <div className={`d-flex flex-column ${styles.appContainer}`}>
       <Header />
       <div className="flex-fill">
-        <Homepage />
-        <Profile />
+        <Outlet />
       </div>
       <Footer />
     </div>
--- src/router.js
+++ src/router.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
+import Homepage from './components/Homepage/Homepage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Homepage />,
+      },
+    ],
   },
 ]);
