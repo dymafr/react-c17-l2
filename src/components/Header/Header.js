17b16604efd5c1dc4d1c0b4f5a8f70adaf1b0db5
dymafr <32801166+dymafr@users.noreply.github.com>
2022-10-06 15:39:40 +0200

    Initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Header.module.scss';
+
+function Header() {
+  return (
+    <header className={`${styles.header} d-flex flex-row align-items-center`}>
+      <div className="flex-fill">
+        <strong> React-router </strong>
+      </div>
+      <ul className={styles.headerList}></ul>
+    </header>
+  );
+}
+
+export default Header;
