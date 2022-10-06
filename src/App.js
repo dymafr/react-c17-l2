fb8127dff321ff0d1780b115631692cee0f70ea1
dymafr <32801166+dymafr@users.noreply.github.com>
2022-10-06 15:46:05 +0200

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Homepage from './pages/Homepage/Homepage';
+import Profile from './pages/Profile/Profile';
 import styles from './App.module.scss';
 
 function App() {
@@ -8,7 +10,8 @@ function App() {
     <div className={`d-flex flex-column ${styles.appContainer}`}>
       <Header />
       <div className="flex-fill">
-        <h1>App</h1>
+        <Homepage />
+        <Profile />
       </div>
       <Footer />
     </div>
